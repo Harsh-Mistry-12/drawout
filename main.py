7f83a6661594c702f5e9c634cf9e70f35e7f0257
harsh-mistry-12 <hmistry864@gmailcom>
2023-12-03 14:27:14 +0530

    try

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from random import randint
+from rembg import remove
+from PIL import Image
+import os.path
+import glob
+import uuid
+import os
+
+IMAGEDIR_UPLOAD = "uploaded_images/"
+IMAGEDIR_CLEANED = "cleaned_images/"
+
+app = FastAPI() 
+
+@app.post("/upload/")
+async def create_upload_file(file: UploadFile = File(...)):
+    file.filename=f"{uuid.uuid4()}"
+    contents = await file.read()
+    
+    with open(f"{IMAGEDIR_UPLOAD}{file.filename}", "wb") as f:
+        f.write(contents)
+        
+    removeBG = Image.open(f"{IMAGEDIR_UPLOAD}{file.filename}")
+    R = remove(removeBG)
+    R.save(f"{IMAGEDIR_CLEANED}{file.filename}.png")
+    os.remove(f"{IMAGEDIR_UPLOAD}{file.filename}")
+
+    return {"filename": file.filename}
+
+@app.get("/show/") # It must return the latest image cleaned 
+async def read_random_file():
+    # files = os.listdir(IMAGEDIR_CLEANED)
+    folder_path = r'cleaned_images/'
+    file_type = r'\*.png'
+    files = glob.glob(folder_path + file_type)
+    max_file = max(files, key=os.path.getctime)
+    # random_index = randint(0, len(files) - 1)
+    
+    # path = f"{IMAGEDIR_CLEANED}{files[0]}"
+    path = f"{max_file}"
+    
+    return FileResponse(path)
